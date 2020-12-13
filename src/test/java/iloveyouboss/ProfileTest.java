6419be6bb4314bde55d2d99f5226237b987f222f
Ryo Utsunomiya <millionsnowliving@gmail.com>
2020-12-13 11:30:12 +0900

    add Profile

---
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -0,0 +1,23 @@
+package iloveyouboss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileTest {
+
+    @Test
+    void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
+        var profile = new Profile("Bull Hockey, Inc.");
+        var question = new BooleanQuestion(1, "ボーナスは支給されますか？");
+        var profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+        var criteria = new Criteria();
+        var criteriaAnswer = new Answer(question, Bool.TRUE);
+        var criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+        assertFalse(matches);
+    }
+}
