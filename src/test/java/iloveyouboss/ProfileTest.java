2c35a1dadb62439378b7791aa7f7851f2b0e2427
Ryo Utsunomiya <millionsnowliving@gmail.com>
2020-12-13 11:33:34 +0900

    add test

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -20,4 +20,19 @@ class ProfileTest {
         boolean matches = profile.matches(criteria);
         assertFalse(matches);
     }
+
+    @Test
+    void matchAnswersTrueForAnyDontCareCriteria() {
+        var profile = new Profile("Bull Hockey, Inc.");
+        var question = new BooleanQuestion(1, "牛乳は支給されますか？");
+        var profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+        var criteria = new Criteria();
+        var criteriaAnswer = new Answer(question, Bool.TRUE);
+        var criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+        assertTrue(matches);
+    }
 }
