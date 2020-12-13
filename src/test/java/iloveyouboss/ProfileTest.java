4b1384486fd0864461c634c390e83324d1665075
Ryo Utsunomiya <millionsnowliving@gmail.com>
2020-12-13 15:34:36 +0900

    refactor test code

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -1,38 +1,42 @@
 package iloveyouboss;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfileTest {
+    Profile profile;
+    BooleanQuestion question;
+    Criteria criteria;
+
+    @BeforeEach
+    void create() {
+        profile = new Profile("Bull Hockey, Inc.");
+        question = new BooleanQuestion(1, "ボーナスは支給されますか？");
+        criteria = new Criteria();
+    }
 
     @Test
+    @DisplayName("必須の条件にマッチしない場合、matchesはfalseを返す")
     void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-        var profile = new Profile("Bull Hockey, Inc.");
-        var question = new BooleanQuestion(1, "ボーナスは支給されますか？");
-        var profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-        var criteria = new Criteria();
-        var criteriaAnswer = new Answer(question, Bool.TRUE);
-        var criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-        criteria.add(criterion);
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
 
         boolean matches = profile.matches(criteria);
+
         assertFalse(matches);
     }
 
     @Test
+    @DisplayName("不問の条件があればmatchesはtrueを返す")
     void matchAnswersTrueForAnyDontCareCriteria() {
-        var profile = new Profile("Bull Hockey, Inc.");
-        var question = new BooleanQuestion(1, "牛乳は支給されますか？");
-        var profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-        var criteria = new Criteria();
-        var criteriaAnswer = new Answer(question, Bool.TRUE);
-        var criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-        criteria.add(criterion);
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
 
         boolean matches = profile.matches(criteria);
+
         assertTrue(matches);
     }
 }
