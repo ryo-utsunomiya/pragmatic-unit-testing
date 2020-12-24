1e72368d9be82e6261976bd5a5ae35f96c7e82d8
Ryo Utsunomiya <millionsnowliving@gmail.com>
2020-12-24 21:42:04 +0900

    add corner case tests

--- src/main/java/iloveyouboss/ScoreCollection.java
+++ src/main/java/iloveyouboss/ScoreCollection.java
@@ -11,14 +11,20 @@ package iloveyouboss;
 import java.util.*;
 
 public class ScoreCollection {
-    private List<Scoreable> scores = new ArrayList<>();
+    private final List<Scoreable> scores = new ArrayList<>();
 
     public void add(Scoreable scoreable) {
+        if (scoreable == null) {
+            throw new IllegalArgumentException();
+        }
         scores.add(scoreable);
     }
 
     public int arithmeticMean() {
-        int total = scores.stream().mapToInt(Scoreable::getScore).sum();
-        return total / scores.size();
+        if (scores.size() == 0) {
+            return 0;
+        }
+        long total = scores.stream().mapToLong(Scoreable::getScore).sum();
+        return (int)(total / scores.size());
     }
 }
--- src/test/java/iloveyouboss/ScoreCollectionTest.java
+++ src/test/java/iloveyouboss/ScoreCollectionTest.java
@@ -1,19 +1,48 @@
 package iloveyouboss;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreCollectionTest {
+    ScoreCollection collection;
+
+    @BeforeEach
+    void setup() {
+        collection = new ScoreCollection();
+    }
 
     @Test
     void answerArithmeticMeanOfTwoNumbers() {
-        var collection = new ScoreCollection();
         collection.add(() -> 5);
         collection.add(() -> 7);
 
         int actual = collection.arithmeticMean();
 
         assertEquals(6, actual);
+    }
+
+    @Test
+    @DisplayName("nullを追加すると例外が発生する")
+    void throwsExceptionWhenAddingNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            collection.add(null);
+        });
+    }
+
+    @Test
+    @DisplayName("何も追加されていない場合はゼロを返す")
+    void answersZeroWhenNoElementsAdded() {
+        assertEquals(0, collection.arithmeticMean());
+    }
+
+    @Test
+    @DisplayName("Integerとしてのオーバーフローに対処する")
+    void dealsWithIntegerOverflow() {
+        collection.add(() -> Integer.MAX_VALUE);
+        collection.add(() -> 1);
+        assertEquals(1073741824, collection.arithmeticMean());
     }
 }
